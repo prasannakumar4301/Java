0196c6dda29e598df9abf0ea76d52b04fb29cbac
prasannakumar4301 <prasannakumar4301@gmail.com>
2023-12-12 17:33:55 +0530

    v1

---
+++ FirstApp.java
@@ -0,0 +1,6 @@
+class FirstApp{
+  static public void main(String args[]){
+       //System.out.println("Hello!");
+       System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+    }
+}
--- LocalVariables.java
+++
@@ -1,7 +0,0 @@
-public class LocalVariables {
-
-    public static void main(String[] args) {
-        int Count = 100;
-        System.out.println(Count);
-    }
-}
